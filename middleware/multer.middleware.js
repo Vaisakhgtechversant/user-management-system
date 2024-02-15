5e697927103e17ed05159e434433de1bc719e7bc
vaisakhgtechversant <vaisakhg@techversantinfotech.com>
2024-02-15 10:37:18 +0530

    fifth  commit

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,39 +1,38 @@
 const multer = require('multer');
 const path = require('path');
 
-const storage = multer.memoryStorage({
+const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, './images');
   },
   filename: (req, file, cb) => {
     const fileExtension = path.extname(file.originalname);
     const fileName = 'Image';
+    // Construct the filename with the date and the original file extension
     const newFileName = `${fileName}-${Date.now()}${fileExtension}`;
     req.body.filename = newFileName;
     cb(null, newFileName);
   },
 });
+
 const upload = multer({
   storage,
-  fileFilter: (res, file, cb) => {
-    try {
-      if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
-        cb(null, true);
-      } else {
-        cb(null, false);
-        cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
-      }
-    } catch (error) {
-      res.stataus(400).json({
-        status: false,
-        message: 'Only .png, .jpg and .jpeg format allowed!',
-      });
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
     }
   },
 });
+
+const UploadPostImage = upload.array('PostImage');
 const UploadImage = upload.array('image');
 const UploadProfileImage = upload.array('image');
+
 module.exports = {
   UpdateGroupImage: UploadImage,
+  UploadPost: UploadPostImage,
   UploadImage: UploadProfileImage,
 };
--- routes/img.router.js
+++ routes/img.router.js
@@ -5,6 +5,7 @@ const imgController = require('../controllers/img.controller');
 const router = express.Router();
 const UploadPost = require('../middleware/multer.middleware');
 
-router.post('/upload', UploadPost.UploadImage, imgController.saveImg);
+router.post('/upload', UploadPost.UploadImage, imgController.saveBlog);
+router.get('/getimage/:filename', imgController.getimage);
 
 module.exports = router;
