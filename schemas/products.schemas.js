8b027e553c926fa1e02acf5fe316cc84262b0eff
vaisakhgtechversant <vaisakhg@techversantinfotech.com>
2024-03-20 12:23:22 +0530

    change product schemas

--- schemas/products.schemas.js
+++ schemas/products.schemas.js
@@ -2,25 +2,25 @@ const Joi = require('@hapi/joi');
 
 module.exports = Joi.object({
   productName: Joi.string()
-    .pattern(/^[a-zA-Z0-9 ]+$/).message('First name must contain only alphabetic characters')
+    .pattern(/^[a-zA-Z0-9 ]+$/).message('only allow charaters and numbers')
     .min(1),
   productPrice: Joi.string()
-    .pattern(/^[a-zA-Z0-9 ]+$/).message('last name must contain only alphabetic characters')
+    .pattern(/^[0-9 ]+$/).message('only allow numbers')
     .min(1),
   productDetails: Joi.string()
-    .pattern(/^[a-zA-Z0-9 ]+$/).message('last name must contain only alphabetic characters')
+    .pattern(/^[a-zA-Z0-9 ]+$/).message('only allow charaters and numbers')
     .min(1),
   category: Joi.string()
-    .pattern(/^[a-zA-Z0-9 ]+$/).message('last name must contain only alphabetic characters')
+    .pattern(/^[a-zA-Z0-9 ]+$/).message('only allow charaters and numbers')
     .min(1),
   availability: Joi.string()
-    .pattern(/^[a-zA-Z0-9 ]+$/).message('last name must contain only alphabetic characters')
+    .pattern(/^[a-zA-Z ]+$/).message('only allow charaters')
     .min(1),
   productCode: Joi.string()
-    .pattern(/^[a-zA-Z0-9 ]+$/).message('last name must contain only alphabetic characters')
+    .pattern(/^[a-zA-Z0-9 ]+$/).message('only allow charaters and letters')
     .min(1),
   quantity: Joi.string()
-    .pattern(/^[a-zA-Z0-9 ]+$/).message('last name must contain only alphabetic characters')
+    .pattern(/^[a-zA-Z0-9 ]+$/).message('only allow numbers')
     .min(1),
   imageURL: Joi.string(),
 });
